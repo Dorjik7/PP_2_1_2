452334b1e25f7a2769795ebb05b2a37947107cb0
Maria Dryukova <mdryukova@yandex.ru>
2022-09-14 15:21:54 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String catVoice;
+
+    public String getCatVoice() {
+        return catVoice;
+    }
+
+    public void setCatVoice(String catVoice) {
+        this.catVoice = catVoice;
+    }
+    public void catVoice(){
+    System.out.println("Мяу-мяу");
+}
+
+}
